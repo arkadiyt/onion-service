68155fc41e9a1de84dbc9dbdae42b4d1964a092d
Arkadiy Tetelman <arkadiyt@gmail.com>
2020-04-14 19:53:12 -0700

    Downgrade openssl to fix RNG error

--- tor/build.sh
+++ tor/build.sh
@@ -3,7 +3,7 @@ set -e
 
 docker build . --compress \
   --build-arg LIBEVENT_VERSION=2.1.11 \
-  --build-arg OPENSSL_VERSION=1.1.1f \
+  --build-arg OPENSSL_VERSION=1.1.1c \
   --build-arg TOR_VERSION=0.4.2.7 \
   --build-arg TOR_USER=tor \
   --build-arg ZLIB_VERSION=1.2.11 \
