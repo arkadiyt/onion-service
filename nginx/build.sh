aea9683b6c738caec924c616ef2f38e62ddd8a12
Arkadiy Tetelman <arkadiyt@gmail.com>
2020-04-14 19:36:40 -0700

    Fixes

--- nginx/build.sh
+++ nginx/build.sh
@@ -4,7 +4,7 @@ set -e
 docker build . --compress \
   --build-arg NGINX_USER=nginx \
   --build-arg NGINX_VERSION=1.17.9 \
-  --build-arg OPENSSL_VERSION=1.1.1f \
+  --build-arg OPENSSL_VERSION=1.1.1c \
   --build-arg PCRE_VERSION=8.44 \
   --build-arg ZLIB_VERSION=1.2.11 \
   --tag nginx
--- tor/build.sh
+++ tor/build.sh
@@ -3,6 +3,8 @@ set -e
 
 docker build . --compress \
   --build-arg LIBEVENT_VERSION=2.1.11 \
+  --build-arg OPENSSL_VERSION=1.1.1f \
   --build-arg TOR_VERSION=0.4.2.7 \
   --build-arg TOR_USER=tor \
+  --build-arg ZLIB_VERSION=1.2.11 \
   --tag tor
